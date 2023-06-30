add2bcdc044425879fa9efa8130f02aa21d0a08b
krishnasv.p <krishnasv.p@cgg.gov.in>
2023-06-30 12:10:29 +0530

    removed some dependencies

--- lib/res/components/reusable_widgets/base_scaffold.dart
+++ lib/res/components/reusable_widgets/base_scaffold.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ttd_flutter_app/res/components/colors/colors.dart';
 import 'package:ttd_flutter_app/res/components/images/image_strings.dart';
 
